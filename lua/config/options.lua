299c66319a3c1b26bc1958c2abb0e53f064ee519
kureta <skureta@gmail.com>
2024-07-28 15:33:50 +0300

    Initial customization

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use traditional vim clipboard setup
+vim.opt.clipboard = ""
+
+-- Set swap, undo, and backup files and their locations
+vim.opt.directory = vim.fn.stdpath("data") .. "/swap//"
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo//"
+vim.opt.backup = true
+vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup//"
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 15
---
+++ lua/plugins/custom.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme("catppuccin-macchiato")
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi("Comment gui=none")
+    end,
+  },
+}
